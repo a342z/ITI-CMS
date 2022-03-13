39e25379afa6bec38bed38bc0e05abecba3a2a22
hassanOsama720 <hassan.osama720@gmail.com>
2022-03-13 13:51:49 +0200

    some chonges

---
+++ frontend/src/app/medicine/medicine.ts
@@ -0,0 +1,14 @@
+export interface Medicine {
+    _id:number
+    comercial_name:string
+    medical_name:string
+    manufacturer:string
+    price:number
+    image:string
+    type:string
+    form:string
+    usage:any
+    dose:object
+    side_effects:any
+    description:string
+}
