dabc5ca8c8b31aaeec21355dff55ee64d52f7c4b
zaki <abdulrahman.zaki.342@gmail.com>
2022-03-22 21:17:45 +0200

    zaki branch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentTableComponent } from './appointment/appointment-table/appointment-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "appointments", component: AppointmentTableComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+  
---
+++ frontend/src/app/models/appointment.ts
@@ -0,0 +1,7 @@
+export interface Appointment {
+    _id: Number,
+    doctorId: Number,
+    patientId: Number,
+    time:Date,
+    status:String 
+}
